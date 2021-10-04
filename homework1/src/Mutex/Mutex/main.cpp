fa09347e5b2d790a16fe047a9554145877f3eedd
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2021-10-04 14:42:14 +0300

    homework1/Galanin_Pavel : Added prjt "Mutex"

---
+++ homework1/src/Mutex/Mutex/main.cpp
@@ -0,0 +1,41 @@
+#include <windows.h>
+#include <stdio.h>
+
+int a[4];
+HANDLE hThr;	// десткриптер потока
+unsigned long uThrID;
+
+HANDLE hMutex = 0;
+
+void ThreadFunction(void* pParams)
+{
+	int i, n = 0;
+	while (1)
+	{
+		WaitForSingleObject(hMutex, INFINITE);
+		// ...
+		ReleaseMutex(hMutex);
+	}
+}
+
+int main()
+{
+	hMutex = CreateMutex(NULL, FALSE, NULL);
+	hThr = CreateThread(
+		NULL,	// настройка безопасности
+		0,		// индентификатор процесса
+		(LPTHREAD_START_ROUTINE)ThreadFunction,
+		NULL,
+		0,
+		&uThrID
+	);
+
+	for (int era = 0; era <= 20; era++)
+	{
+		WaitForSingleObject(hMutex, 0);
+		printf("%4d era: %8d %8d %8d %8d\n", era, a[0], a[1], a[2], a[3]);
+		ReleaseMutex(hMutex);
+	}
+
+	return 0;
+}
