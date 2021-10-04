5636c0f34d2a34a2fea164a7cecde54d0d8bf075
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2021-10-04 14:08:53 +0300

    homework1/Galanin_Pavel : Add prjt CriticalSection

---
+++ homework1/src/CriticalSection/CriticalSection/main.cpp
@@ -0,0 +1,41 @@
+#include <windows.h>
+#include <stdio.h>
+
+int a[4];
+HANDLE hThr;	// десткриптер потока
+unsigned long uThrID;
+
+CRITICAL_SECTION cs;
+
+void ThreadFunction(void* pParams)
+{
+	int i, n = 0;
+	while(1)
+	{
+		EnterCriticalSection(&cs);
+		// ...
+		LeaveCriticalSection(&cs);
+	}
+}
+
+int main()
+{
+	InitializeCriticalSection(&cs);
+	hThr = CreateThread(
+		NULL,	// настройка безопасности
+		0,		// индентификатор процесса
+		(LPTHREAD_START_ROUTINE)ThreadFunction,
+		NULL,
+		0,
+		&uThrID
+	);
+
+	for (int era = 0; era <= 20; era++)
+	{
+		EnterCriticalSection(&cs);
+		printf("%4d era: %8d %8d %8d %8d\n", era, a[0], a[1], a[2], a[3]);
+		LeaveCriticalSection(&cs);
+	}
+
+	return 0;
+}
