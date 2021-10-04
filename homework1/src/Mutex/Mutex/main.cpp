ed97f15e80a551b58822ad4d25cbb429791ecab7
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2021-10-04 15:14:37 +0300

    homework1/Galanin_Pavel : Updated prjt "Mutex"

--- homework1/src/Mutex/Mutex/main.cpp
+++ homework1/src/Mutex/Mutex/main.cpp
@@ -13,7 +13,11 @@ void ThreadFunction(void* pParams)
 	while (1)
 	{
 		WaitForSingleObject(hMutex, INFINITE);
-		// ...
+		for (i = 0; i < 4; i++)
+		{
+			a[i] = n;
+		}
+		n++;
 		ReleaseMutex(hMutex);
 	}
 }
